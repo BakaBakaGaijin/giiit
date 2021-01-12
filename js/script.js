b1142b884dffb5e903d60572a2b3c3ef9ef7a577
BakaBakaGaijin <legostaevofficial@yandex.ru>
2021-01-12 14:14:55 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,66 @@
+let user = {
+  firstName: "Вася",
+};
+
+function func() {
+  console.log(this.firstName);
+}
+
+let funcUser = func.bind(user);
+funcUser();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Menu {
+//   constructor(elem) {
+//     this._elem = elem;
+//     elem.onclick = this.onClick.bind(this);
+//   }
+
+//   save() {
+//     alert("Сохраняю");
+//   }
+
+//   load() {
+//     alert("Загружаю");
+//   }
+
+//   search() {
+//     alert("Ищу");
+//   }
+
+//   onClick(event) {
+//     let action = event.target.dataset.action;
+//     if (action) {
+//       this[action]();
+//     }
+//   };
+// }
+
+// new Menu(menu);
